7c3ade3b468d812b9d86d295b8d71de47678e233
aikdp <kdprasad028@gmail.com>
2025-02-01 09:40:36 +0530

    CICD tools jenkins-pipeline

--- data.tf
+++ data.tf
@@ -16,3 +16,24 @@ data "aws_ami" "devops" {
         values = ["hvm"]
     }
 }
+
+#ami-0c1a611315833061b
+#You need to subscribe SonarQube 
+data "aws_ami" "sonarqube" {
+    most_recent = true
+    owners = ["679593333241"]
+
+    filter {
+        name = "name" 
+        values = ["SolveDevOps-SonarQube-Server-Ubuntu20.04-20250113-fae7c0cd-e3336ad7-93a2-4e7c-8ed8-227cfbf25da4"]
+    }    
+
+    filter {
+        name = "root-device-type" 
+        values = ["ebs"]
+    }
+    filter {
+        name = "virtualization-type" 
+        values = ["hvm"]
+    }
+}
--- main.tf
+++ main.tf
@@ -46,6 +46,30 @@ module "jenkins_agent" {
     ]
 }
 
+# #Create SonarQube instance
+# module "sonarqube" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   ami = data.aws_ami.devops.id
+#   name = "sonarqube"
+
+#   instance_type          = "t3.small"
+#   vpc_security_group_ids = ["sg-0f487d954cbe820ef"]
+#   subnet_id              = "subnet-0f7c76cdf5db0d0b8"
+
+
+#   tags ={
+#           Name = "sonarqube"
+#         }
+    
+#    root_block_device = [
+#        {
+#         volume_size = 50     # Size of the root volume in GB
+#         volume_type = "gp3"  # General Purpose SSD (you can change it if needed)
+#         delete_on_termination = true    # Automatically delete the volume when the instance is terminated
+#        }
+#     ]
+# }
+
 #Creating records for jenkins
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
@@ -72,5 +96,23 @@ module "records" {
       ]
       allow_overwrite = true
     },
+    #   {
+    #   name    = "sonar-public"
+    #   type    = "A"
+    #   ttl     = 1
+    #   records = [
+    #     module.sonarqube.public_ip,
+    #   ]
+    #   allow_overwrite = true
+    # },
+    #   {
+    #   name    = "sonar-private"
+    #   type    = "A"
+    #   ttl     = 1
+    #   records = [
+    #     module.sonarqube.private_ip,
+    #   ]
+    #   allow_overwrite = true
+    # },
   ]
 }
